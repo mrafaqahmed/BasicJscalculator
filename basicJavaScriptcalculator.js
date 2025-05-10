d1622979bb2679e63f4670d8f08c140ae5d78fac
mrafaqahmed <55241@students.riphah.edu.pk>
2025-05-10 15:34:46 +0500

    Add files via upload

---
+++ basicJavaScriptcalculator.js
@@ -0,0 +1,28 @@
+// Defining the function:
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1,num2)
+{
+    return num1-num2;
+}
+function mul(num1,num2)
+{
+    return num1*num2;
+}
+function div(num1,num2)
+{
+    return num1/num2;
+}
+function mod(num1,num2)
+{
+    return num1%num2;
+}
+
+// Calling the function:
+console.log(sum(3, 6)); 
+console.log(sub(3, 6)); 
+console.log(mul(3, 6)); 
+console.log(div(3, 6)); 
+console.log(mod(3, 6)); 
